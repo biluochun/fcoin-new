915e1963636239b9086d373ad869b57597593d10
Jombo-k <994249582@qq.com>
2018-09-10 17:22:38 +0800

    header css 1722

--- src/include/Header.vue
+++ src/include/Header.vue
@@ -2,11 +2,11 @@
     <header class="header-container">
         <div class="header-top">
             <div class="header-top-left">
-                <span class="header-top-entry">FT</span>
-                <span class="header-top-entry">白皮书</span>
-                <span class="header-top-entry">资产透明</span>
-                <span class="header-top-entry">FInsur</span>
-                <span class="header-top-entry">FCandy</span>
+                <span class="header-entry header-active-entry ">FT</span>
+                <span class="header-entry">白皮书</span>
+                <span class="header-entry">资产透明</span>
+                <span class="header-entry">FInsur</span>
+                <span class="header-entry">FCandy</span>
             </div>
             <div class="header-top-right">
                 <span class="header-top-entry">App下载</span>
@@ -14,7 +14,14 @@
             </div>
         </div>
         <div class="header-bottom">
-            bottom
+            <div class="header-bottom-left">                
+                <img class="header-logo" src="./logo.svg" alt="">
+                <span class="header-exchange-entry header-active-entry ">主板交易</span>
+                <span class="header-exchange-entry header-active-entry ">FOne交易</span>
+            </div>
+            <div class="header-bottom-right">
+                <span>巴斯光年</span>
+            </div>
         </div>
     </header>
 </template>
@@ -33,10 +40,14 @@
             font-size: 13px;
             .header-top-left {
                 display: inline-block;
-                .header-top-entry {
+                .header-entry {
                     display: inline-block;
-                    margin: 12px 0 11px 30px;
+                    padding: 12px 15px 11px 15px;
                     cursor: pointer;
+                    &:first-child {
+                        padding-left: 27px;    
+                        width: 30px;
+                    }
                 }
             }
             .header-top-right {
@@ -45,11 +56,46 @@
                 .header-top-entry {
                     display: inline-block;
                     margin: 12px 30px 11px 0px;
+                    cursor: pointer;
                 }
             }
         }
         .header-bottom {
-            
+            width: 100%;
+            height: 60px;
+            color: #E9E9E9;
+            font-size: 14px;
+            .header-bottom-left {
+                display: inline-block;
+                .header-logo {
+                    width: 133px;
+                    height: 28px;
+                    margin: 18px 0 0 30px;
+                    cursor: pointer;
+                }
+                .header-exchange-entry {
+                    display: inline-block;
+                    width: 100px;
+                    height: 40px;
+                    line-height: 40px;
+                    margin: 10px;
+                    text-align: center;
+                    vertical-align: top;
+                    cursor: pointer;
+                }
+            }
+            .header-bottom-right {
+                float: right;
+                display: inline-block;
+                margin-right: 30px;
+                line-height: 60px;
+                font-size: 13px;
+                cursor: pointer;
+            }
+        }
+        .header-active-entry {
+            background: #394A53;
+            color: #E9E9E9;
         }
     }
 </style>
