10abe9c67ff0c177f9aecdff6ac9ed107b32f65e
fuheyong <2545497785@qq.com>
2018-09-12 18:28:31 +0800

    app

--- src/view/index/app/index.vue
+++ src/view/index/app/index.vue
@@ -1,14 +1,25 @@
 <template>
     <div class="module-app">
-        {{$t('app.title')}}
+       <div class="layout-container app-container">
+           <div class="down-icon">
+               <img src="./img/app_icon.png" alt="">
+           </div>
+           <div class="app-title">
+               <p class="app-text">随时随地交易，快人一步成交你</p>
+               <p class="app-system">扫码后，连接VPN下载Android&IOS</p>
+           </div>
+           <div class="down-code">
+               
+           </div>
+       </div>
     </div>
 </template>
 
 <script>
     /**
      * @file
-     * @author jinguangguo
-     * @date 2018/9/7 下午7:33
+     * @author fuheyong
+     * @date 2018/9/12 下午7:33
      */
 
     import vm from './main.js';
