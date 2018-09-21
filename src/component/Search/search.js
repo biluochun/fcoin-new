21244890a6c6e41c33003ad0029c1e4fc1b41ebf
Jombo-k <994249582@qq.com>
2018-09-21 19:50:53 +0800

    search select open and close

--- src/component/Search/search.js
+++ src/component/Search/search.js
@@ -2,9 +2,11 @@
  * @Author: liujikuan 
  * @Date: 2018-09-20 15:14:16 
  * @Last Modified by: liujikuan
- * @Last Modified time: 2018-09-20 18:05:11
+ * @Last Modified time: 2018-09-21 19:50:24
  */
 import lang from '../../common/commonLang'
+import $ from "jquery"
+
 export default {
     name: 'Search',
     i18n: {
@@ -19,13 +21,39 @@ export default {
     created() {
 
     },
+    mounted() {
+        $(document).click(() => {
+            this.showSelect = false
+        });
+    },
     data(){
         return {
-
+            isSymbolSelect: false,
+            showSelect: false,
         }
     },
     methods: {
-
+        /**
+         * 点击选择币种机构
+         */
+        showType: function($event) {
+            
+            $event.stopPropagation();
+            this.showSelect = !this.showSelect
+        },
+        /**
+         * 显示搜索类型文字
+         * @param key
+         */
+        setType(key) {
+            if (key === 'symbol') {
+                this.isSymbolSelect = true
+            } else {
+                this.isSymbolSelect = false
+            }
+            this.showSelect = !this.showSelect
+            $(this.$refs.searchInput).focus()
+        },
     }
 
 }
